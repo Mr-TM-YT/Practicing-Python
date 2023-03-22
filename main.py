cf4cbb732faaf8160b9e4fa364d1238c86ee7a53
Mohamed Ashraf <www.mohamedsalwa13@gmail.com>
2023-03-22 22:18:43 +0200

    Mohamed made a basic Calculator

--- main.py
+++ main.py
@@ -2,6 +2,34 @@ print("This is a Test")
 print("From Mohamed")
 print("Moahemd did clone the Repo")
 
+
+# Start of Basic Calculator
+
+num1 = int(input("Enter your first numers: "))
+operator = input("Enter the operator: ")
+num2 = int((input("Enter your second number: ")))
+
+
+if operator == '+':
+    print(num1 + num2)
+elif operator == '-':
+    if (num1 - num2)  >= 0:
+        print(num1 - num2)
+    else:
+        print("It's a negative value")
+elif operator == '÷' or operator == '/':
+    if num1 == 0:
+        print("Math error (No deviding by 0)")
+    else:
+        print(num1 / num2)
+elif operator == '*' or operator == 'x':
+    print(num1 * num2)
+else:
+    print("You maybe did undefined operator or nonsense number")
+
+
+# End of Basic Calculator
+
 """
   Ahmed have to print From Ahmed
   Then we will talk
