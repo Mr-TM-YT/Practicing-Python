cc230f34563ad951dd156b3b9a96e30c93c8e2fe
Mr-TM-YT <114176526+Mr-TM-YT@users.noreply.github.com>
2023-03-23 06:28:40 +0000

    Made the Calculator in a function and everything in a main function

--- main.py
+++ main.py
@@ -1,30 +1,29 @@
+def main():
+  Calculator()
 
-# Start of Basic Calculator
+def Calculator():
+  num1 = int(input("Enter your first number: "))
+  operator = input("Enter the operator: ")
+  num2 = int(input("Enter your second number: "))
+  
+  if operator == '+':
+      print(num1 + num2)
+  elif operator == '-':
+      if (num1 - num2)  >= 0:
+          print(num1 - num2)
+      else:
+          print("It's a negative value")
+  elif operator == '÷' or operator == '/':
+      if num1 == 0:
+          print("Math error (No deviding by 0)")
+      else:
+          print(num1 / num2)
+  elif operator == '*' or operator == 'x':
+      print(num1 * num2)
+  else:
+      print("You maybe did undefined operator or nonsense number")
 
-num1 = int(input("Enter your first number: "))
-operator = input("Enter the operator: ")
-num2 = int(input("Enter your second number: "))
-
-
-if operator == '+':
-    print(num1 + num2)
-elif operator == '-':
-    if (num1 - num2)  >= 0:
-        print(num1 - num2)
-    else:
-        print("It's a negative value")
-elif operator == '÷' or operator == '/':
-    if num1 == 0:
-        print("Math error (No deviding by 0)")
-    else:
-        print(num1 / num2)
-elif operator == '*' or operator == 'x':
-    print(num1 * num2)
-else:
-    print("You maybe did undefined operator or nonsense number")
-
-
-# End of Basic Calculator
+main()
 
 """
   Ahmed have to print From Ahmed
